32dbc8b13acc30b9a7276d6a77d81b6a7dc3e809
Harshal Bhatia <7516211+harshalbhatia@users.noreply.github.com>
2026-05-27 02:02:02 -0400

    release 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.harshalbhatia.whatsopen"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
